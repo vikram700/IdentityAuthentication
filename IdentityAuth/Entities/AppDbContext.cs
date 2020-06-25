f21f4555df63cd532ebed04340abf746501ceab6
Vikram Singh <vikramsinghshekhawat442@gmail.com>
2020-06-25 09:40:40 +0530

    Add project files.

---
+++ IdentityAuth/Entities/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityAuth.Entities
+{
+    public class AppDbContext : IdentityDbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) 
+            : base(options)
+        {
+
+        }
+    }
+}
---
+++ IdentityAuth/Models/RegisterUser.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityAuth.Models
+{
+    public class RegisterUser
+    {   
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        [Compare("Password",
+            ErrorMessage = "Password and Confirm Password do not match")]
+        public string ConfirmPassword { get; set; }
+    }
+}
